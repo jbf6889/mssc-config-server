a4028d7bc409102f2f50cee6be72c0868a6b9b55
jbf6889 <jfarmeriv@gmail.com>
2020-05-13 04:09:46 -0400

    Initial Checkin

---
+++ src/test/java/com/sfg/msscconfigserver/MsscConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfg.msscconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
